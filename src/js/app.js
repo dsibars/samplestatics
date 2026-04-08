003615e6cbe5b0332780d5030701710c895e38e3
Developer <developer@example.com>
2026-04-08 15:05:48 +0200

    fix: swapped html5 drag/drop for mobile-first sortablejs

--- src/js/app.js
+++ src/js/app.js
@@ -2,7 +2,7 @@ import '../css/base.css';
 import '../css/app.css';
 
 import { applyTranslations, changeLanguage, t } from './i18n.js';
-import { updateActiveDate, resetToToday, showSettings, closeSettings, exportJSON, importJSON, clearAllData, showExit, showRoutine, closeRoutine, routineDragStart, routineDragOver, routineDragEnter, routineDragLeave, routineDrop, removeRoutineStep, openAddExercise, closeAddExercise, updateExercisePreview, appendNewExercise } from './configuration.js';
+import { updateActiveDate, resetToToday, showSettings, closeSettings, exportJSON, importJSON, clearAllData, showExit, showRoutine, closeRoutine, openAddExercise, closeAddExercise, updateExercisePreview, appendNewExercise } from './configuration.js';
 import { startWorkout, processStep, changeVal } from './workout.js';
 import { showHistory, toggleCard } from './statistics.js';
 
@@ -29,11 +29,6 @@ window.updateActiveDate = updateActiveDate;
 window.resetToToday = resetToToday;
 window.showRoutine = showRoutine;
 window.closeRoutine = closeRoutine;
-window.routineDragStart = routineDragStart;
-window.routineDragOver = routineDragOver;
-window.routineDragEnter = routineDragEnter;
-window.routineDragLeave = routineDragLeave;
-window.routineDrop = routineDrop;
 window.removeRoutineStep = removeRoutineStep;
 window.openAddExercise = openAddExercise;
 window.closeAddExercise = closeAddExercise;
